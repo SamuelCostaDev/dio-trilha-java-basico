ea877abd6104a21ffae79c72042c230ae64ae201
SamuelCostaDev <dev.samuelcosta@gmail.com>
2024-05-06 17:44:09 -0300

    Operadores-7

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,12 +1,12 @@
 public class Operadores {
     public static void main(String [] args) {
-        int a, b;
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+        
+        if (condicao1 || condicao2) {
+            System.out.println("As duas condições são verdadeiras");
+        }
 
-        a = 6;
-        b = 6;
-
-        String resultado = a == b ? "verdadeiro" : "falso";
-
-        System.out.println(resultado);
+        System.out.println("Fim");
     }
 }
